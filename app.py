f3eb694a8d586346d08a491655762d9c2146d264
tddev123 <154409493+tddev123@users.noreply.github.com>
2025-02-05 03:40:55 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,42 +1,32 @@
-print("Welcome to NeuralNine YouTube Downloader and Converter v0.2 Alpha")
-print("Loading...")
+const convertButton = document.querySelector('.button-convert');
+const inputURL = document.querySelector('.input-url');
+const format = document.getElementById('format');
+const quality = document.getElementById('quality');
 
-import pytube
-import youtube_downloader
-import file_converter
+const serverURL = 'http://localhost:8000';
 
-print('''
-What do you want?
+convertButton.addEventListener('click', () => {
+    const selectedFormat = format[format.selectedIndex].value;
+    const selectedQuality = quality[quality.selectedIndex].value;
+    if (inputURL.value.trim().length > 0) {
+        sendButton(inputURL.value, selectedFormat, selectedQuality);
+    }
+});
 
-(1) Download YouTube Videos Manually
-(2) Download a YouTube Playlist
-(3) Download YouTube Videos and Convert Into MP3
-
-Downloading copyrighted YouTube videos is illegal!
-I am not responsible for your downloads! Go at your own risk!
-
-Copyright (c) NeuralNine 2020
-''')
-
-choice = input("Choice: ")
-
-if choice == "1" or choice == "2":
-    quality = input("Please choose a quality (low, medium, high, very high):")
-    if choice == "2":
-        link = input("Enter the link to the playlist: ")
-        print("Downloading playlist...")
-        youtube_downloader.download_playlist(link, quality)
-        print("Download finished!")
-    if choice == "1":
-        links = youtube_downloader.input_links()
-        for link in links:
-            youtube_downloader.download_video(link, quality)
-elif choice == "3":
-    links = youtube_downloader.input_links()
-    for link in links:
-        print("Downloading...")
-        filename = youtube_downloader.download_video(link, 'low')
-        print("Converting...")
-        file_converter.convert_to_mp3(filename)
-else:
-    print("Invalid input! Terminating...")
+const sendButton = (videoURL, format, quality) => {
+    fetch(`${serverURL}/check-download?URL=${videoURL}`)
+        .then(response => response.json())
+        .then(resData => {
+            const data = JSON.parse(JSON.stringify(resData));
+            if (data.status === true) {
+                document.getElementById(
+                    'downloading',
+                ).innerHTML = `Starting the download of ${data.title} by ${
+                    data.author
+                    }...`;
+                window.location.href = `${serverURL}/download?URL=${videoURL}&downloadFormat=${format}&quality=${quality}&title=${
+                    data.title
+                    }`;
+            }
+        });
+};
